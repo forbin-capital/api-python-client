85cb47dd298e78d8ec6d53cd1d69ca0a26272c2b
Edouard d'Archimbaud <edouard@myaiplatform.com>
2021-11-11 19:38:58 +0100

    Working client

--- forbin/client.py
+++ forbin/client.py
@@ -1,41 +1,158 @@
-import pandas as pd
-import requests
+from datetime import datetime
 from typing import Union
 
+import pandas as pd
+import pytz
+import requests
 
-class Challenge:
-    def __init__(self, data=None, dataset_id=None, submission_start_date=None,
-                 submission_end_date=None, x=None, y=None) -> None:
+
+class Object:
+    def __init__(self, client=None, data=None) -> None:
+        self._client = client
         self.id = data.get('id')
-        self.dataset_id = data.get('dataset_id', dataset_id)
-        self.submission_start_date = data.get('submission_start_date', submission_start_date)
-        self.submission_end_date = data.get('submission_end_date', submission_end_date)
-        self.x = pd.DataFrame(data=data.get('x', x))
-        self.y = pd.DataFrame(data=data.get('y', y))
+        self.created_at = datetime.fromisoformat(
+            data.get('created_at')[:-1]).replace(tzinfo=pytz.UTC)
+    
+    def __str__(self) -> str:
+        return {
+            'id': self.id,
+            'created_at': self.created_at,
+        }.__str__()
+    
+    def delete(self) -> None:
+        """
+        Delete object.
+        Parameters:
+        -----------
+        Returns:
+        --------
+        """
+        self._client._delete(route=f'{self._subroute}/{self.id}')
+    
+    def save(self):
+        """
+        Save object.
+        Parameters:
+        -----------
+        Returns:
+        --------
+        """
+        if self.id:
+            return self._client._patch(route=f'{self._subroute}/{self.id}', data=self.__dict__())
+        return self._client._post(route=f'{self._subroute}', data=self.__dict__())
+        
 
 
+class Challenge(Object):
+    
+    _subroute = 'challenges'
 
-class Dataset:
-    def __init__(self, data=None, date=None, x=None, y=None) -> None:
-        self.id = data.get('id')
-        self.date = data.get('date', date)
-        self.x = pd.DataFrame(data=data.get('x', x))
-        self.y = pd.DataFrame(data=data.get('y', y))
+    def __init__(self, client=None, data={}) -> None:
+        super().__init__(client, data)
+        self.finished = data.get('finished')
+        self.prize = data.get('prize')
+        self.submission_start = datetime.fromisoformat(
+            data.get('submission_start')[:-1]).replace(tzinfo=pytz.UTC)
+        self.submission_end = datetime.fromisoformat(
+            data.get('submission_end')[:-1]).replace(tzinfo=pytz.UTC)
+        self.x_live = pd.DataFrame(data=data.get('x_live'))
+        self.x_test = pd.DataFrame(data=data.get('x_live'))
+        self.x_train = pd.DataFrame(data=data.get('x_live'))
+        self.y_train = pd.DataFrame(data=data.get('x_live'))
+    
+    def __dict__(self) -> dict:
+        return {
+            'id': self.id,
+            'created_at': self.created_at.isoformat(),
+            'finished': self.finished,
+            'prize': self.prize,
+            'submission_start': self.submission_start.isoformat(),
+            'submission_end': self.submission_end.isoformat(),
+            'x_live': self.x_live.to_dict(orient='records'),
+            'x_test': self.x_test.to_dict(orient='records'),
+            'x_train': self.x_train.to_dict(orient='records'),
+            'y_train': self.y_train.to_dict(orient='records'),
+        }
+    
+    
 
+class GroundTruth(Object):
 
-class Submission:
-    def __init__(self, data=None, challenge_id=None, submission_date=None,
-                 user_id=None, y=None) -> None:
-        self.id = data.get('id')
-        self.challenge_id = data.get('challenge_id', challenge_id)
-        self.submission_date = data.get('submission_date', submission_date)
-        self.user_id = pd.DataFrame(data=data.get('user_id', user_id))
-        self.y = pd.DataFrame(data=data.get('y', y))
+    _subroute = 'groundtruths'
+
+    def __init__(self, client=None, data={}) -> None:
+        super().__init__(client, data)
+        self.challenge_id = data.get('challenge_id')
+        self.y_live = pd.DataFrame(data=data.get('y_live'))
+        self.y_test = pd.DataFrame(data=data.get('y_test'))
+    
+    def __dict__(self) -> dict:
+        return {
+            'id': self.id,
+            'created_at': self.created_at.isoformat(),
+            'challenge_id': self.challenge_id,
+            'y_live': self.y_live.to_dict(orient='records'),
+            'y_test': self.y_test.to_dict(orient='records'),
+        }
+
+class Submission(Object):
+    
+    _subroute = 'submissions'
+
+    def __init__(self, client=None, data={}) -> None:
+        super().__init__(client, data)
+        self.challenge_id = data.get('challenge_id')
+        self.confidence = data.get('confidence')
+        self.consistency = data.get('consistency')
+        self.logloss = data.get('logloss')
+        self.originality = data.get('originality')
+        self.reward = data.get('reward')
+        self.stake = data.get('stake')
+        self.user_id = data.get('user_id')
+        self.y_live = pd.DataFrame(data=data.get('y_live'))
+        self.y_test = pd.DataFrame(data=data.get('y_test'))
+    
+    def __dict__(self) -> dict:
+        return {
+            'id': self.id,
+            'created_at': self.created_at.isoformat(),
+            'challenge_id': self.challenge_id,
+            'confidence': self.confidence,
+            'consistency': self.consistency,
+            'logloss': self.logloss,
+            'originality': self.originality,
+            'reward': self.reward,
+            'stake': self.stake,
+            'user_id': self.user_id,
+            'y_live': self.y_live.to_dict(orient='records'),
+            'y_test': self.y_test.to_dict(orient='records'),
+        }
+
+class Transaction(Object):
+    
+    _subroute = 'transactions'
+
+    def __init__(self, client=None, data={}) -> None:
+        super().__init__(client, data)
+        self.challenge_id = data.get('challenge_id')
+        self.amount = data.get('amount')
+        self.category = data.get('category')
+        self.user_id = data.get('user_id')
+    
+    def __dict__(self) -> dict:
+        return {
+            'id': self.id,
+            'created_at': self.created_at.isoformat(),
+            'challenge_id': self.challenge_id,
+            'amount': self.amount,
+            'category': self.category,
+            'user_id': self.user_id,
+        }
 
 
 class Client:
 
-    def __init__(self, username, password, api_endpoint='http://localhost:8000/api'):
+    def __init__(self, username, password, api_endpoint='https://app.forbin-capital.com/api'):
         self.api_endpoint = api_endpoint
         self.token = None
         response = requests.post(
@@ -44,7 +161,7 @@ class Client:
             return print(response.json())
         self.token = response.json()['token']
     
-    def _delete(self, route, data):
+    def _delete(self, route):
         response = requests.delete(
             f'{self.api_endpoint}/{route}/', headers={'Authorization': f'Token {self.token}'})
         if response.status_code != 200:
@@ -60,75 +177,83 @@ class Client:
     
     def _patch(self, route, data):
         response = requests.patch(
-            f'{self.api_endpoint}/{route}/', data=data, headers={'Authorization': f'Token {self.token}'})
+            f'{self.api_endpoint}/{route}/', json=data, headers={'Authorization': f'Token {self.token}'})
         if response.status_code != 200:
             return print(response.json())
         return response.json()
     
     def _post(self, route, data):
         response = requests.post(
-            f'{self.api_endpoint}/{route}/', data=data, headers={'Authorization': f'Token {self.token}'})
+            f'{self.api_endpoint}/{route}/', json=data, headers={'Authorization': f'Token {self.token}'})
         if response.status_code != 200:
             return print(response.json())
         return response.json()
 
-    @property
-    def challenge(self) -> Union[Challenge, list[Challenge]]:
+    def challenges(self, id=None) -> Union[Challenge, list[Challenge]]:
+        """
+        Get challenges.
+        Parameters:
+        -----------
+        id : string
+            Challenge ID.
+        Returns:
+        --------
+        Either a list of Challenge objects or one Challenge object.
+        """
         data = self._get(route='challenges')
         if len(data) == 1:
-            return Challenge(data[-1])
-        return [Challenge(d) for d in data]
+            return Challenge(client=self, data=data[0])
+        return [Challenge(client=self, data=d) for d in data]
     
-    @challenge.deleter
-    def challenge(self, value: Challenge):
-        return self._delete(route=f'challenges/{value.id}')
-
-    @challenge.setter
-    def challenge(self, value: Challenge):
-        data = {
-            'dataset_id': value.dataset_id,
-            'submission_start_date': value.submission_start_date,
-            'submission_end_date': value.submission_end_date,
-            'x': value.x.to_dict(orient='records'),
-            'y': value.y.to_dict(orient='records')
-        }
-        if value.id:
-            return self._patch(route=f'challenges/{value.id}', data=data)
-        return self._post(route='challenges', data=data)
-
-    @property
-    def dataset(self) -> Union[Dataset, list[Dataset]]:
-        data = self._get(route='datasets')
+    def ground_truths(self, id=None) -> Union[GroundTruth, list[GroundTruth]]:
+        """
+        Get ground truths.
+        Parameters:
+        -----------
+        id : string
+            GroundTruth ID.
+        Returns:
+        --------
+        Either a list of GroundTruth objects or one GroundTruth object.
+        """
+        data = self._get(route='groundtruths')
         if len(data) == 1:
-            return Dataset(data[-1])
-        return [Dataset(d) for d in data]
+            return GroundTruth(client=self, data=data[0])
+        return [GroundTruth(client=self, data=d) for d in data]
     
-    @dataset.setter
-    def dataset(self, value: Dataset):
-        data = {
-            'date': value.date.isoformat(),
-            'x': value.x.to_dict(orient='records'),
-            'y': value.y.to_dict(orient='records')
-        }
-        return self._post(route='datasets', data=data)
-
-    @property
-    def submission(self) -> Union[Submission, list[Submission]]:
+    def submissions(self, id=None) -> Union[Submission, list[Submission]]:
+        """
+        Get submissions.
+        Parameters:
+        -----------
+        id : string
+            Submission ID.
+        Returns:
+        --------
+        Either a list of Submission objects or one Submission object.
+        """
         data = self._get(route='submissions')
         if len(data) == 1:
-            return Submission(data[-1])
-        return [Submission(d) for d in data]
+            return Submission(client=self, data=data[0])
+        return [Submission(client=self, data=d) for d in data]
+    
+    def transactions(self, id=None) -> Union[Transaction, list[Transaction]]:
+        """
+        Get transactions.
+        Parameters:
+        -----------
+        id : string
+            Transaction ID.
+        Returns:
+        --------
+        Either a list of Transaction objects or one Transaction object.
+        """
+        data = self._get(route='transactions')
+        if len(data) == 1:
+            return Transaction(client=self, data=data[0])
+        return [Transaction(client=self, data=d) for d in data]
+    
+    
+    
 
-    @submission.setter
-    def submission(self, value: Submission):
-        data = {
-            'y': value.y.to_dict(orient='records')
-        }
-        return self._post(route='submissions', data=data)
 
-
-if __name__ == '__main__':
-    client = Client(username='test', password='abc1234!')
-    print(client.dataset)
-    print(client.challenge)
-    print(client.submission)
